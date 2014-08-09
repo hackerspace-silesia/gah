10f1c7ea15439d9a9a9f21b6f534581146ab163b
Kamil Gałuszka <kgaluszka@nglogic.com>
2014-08-09 05:02:18 +0200

    added player class

--- app/main.js
+++ app/main.js
@@ -1,79 +1,72 @@
-var background;
-var player;
-var currentSpeed = 0;
-var cursors;
+function initialize() {
+    var map_element = document.getElementById('map');
+    map_element.style.width = window.outerWidth;
+    map_element.style.height = window.outerHeight;
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', {
-    preload: preload,
-    create: create,
-    update: update,
+    var map_canvas = map_element;
+    var map_options = {
+        center: new google.maps.LatLng(31.5869047, 64.3101938),
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+
+    var map = new google.maps.Map(map_canvas, map_options);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+$('#start').click(function() {
+    $('#splash').remove();
+
+    StartGame();
 });
 
-function preload() {
-    game.load.atlas('player', 'assets/games/tanks/tanks.png', 'assets/games/tanks/tanks.json');
-    game.load.image('background', 'assets/games/tanks/scorched_earth.png');
-}
 
-function create() {
-
-    //  Resize our game world to be a 2000 x 2000 square
-    game.world.setBounds(-1000, -1000, 2000, 2000);
-
-    //  Our tiled scrolling background
-    background = game.add.tileSprite(0, 0, 800, 600, 'earth');
-    background.fixedToCamera = true;
-
-    //  The base of our player
-    player = game.add.sprite(0, 0, 'player', 'tank1');
-    player.anchor.setTo(0.5, 0.5);
-    player.animations.add('move', ['tank1', 'tank2', 'tank3', 'tank4', 'tank5', 'tank6'], 20, true);
-
-    //  This will force it to decelerate and limit its speed
-    game.physics.enable(player, Phaser.Physics.ARCADE);
-    player.body.drag.set(0.2);
-    player.body.maxVelocity.setTo(400, 400);
-    player.body.collideWorldBounds = true;
-
-
-    player.bringToTop();
-
-    game.camera.follow(player);
-    game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300);
-    game.camera.focusOnXY(0, 0);
-
-    cursors = game.input.keyboard.createCursorKeys();
-
-}
-
-function update() {
-
-    if (cursors.left.isDown) {
-        player.angle -= 4;
-    } else if (cursors.right.isDown) {
-        player.angle += 4;
+function StartGame() {
+    var background;
+    var player;
+    var currentSpeed = 0;
+    var cursors;
+    var game_bounds = {
+        'width': window.innerWidth,
+        'height': window.innerHeight
     }
 
-    if (cursors.up.isDown) {
-        //  The speed we'll travel at
-        currentSpeed = 300;
-    } else {
-        if (currentSpeed > 0) {
-            currentSpeed -= 4;
-        }
+
+
+    var game = new Phaser.Game(game_bounds.width, game_bounds.height, Phaser.AUTO, 'phaser-example', {
+        preload: preload,
+        create: create,
+        update: update,
+    });
+
+    function preload() {
+        game.load.atlas('player', 'assets/images/tanks.png', 'assets/data/tanks.json');
+        game.load.image('background', 'assets/images/scorched_earth.png');
     }
 
-    if (currentSpeed > 0) {
-        game.physics.arcade.velocityFromRotation(player.rotation, currentSpeed, player.body.velocity);
+    function create() {
+
+        //  Resize our game world to be a 2000 x 2000 square
+        game.world.setBounds(-1000, -1000, 2000, 2000);
+
+        //  Our tiled scrolling background
+        background = game.add.tileSprite(0, 0, game_bounds.width, game_bounds.height, 'earth');
+        background.fixedToCamera = true;
+        cursors = game.input.keyboard.createCursorKeys();
+        player = new Player(game, cursors);
+        //  This will force it to decelerate and limit its speed
+        game.camera.follow(player.sprite);
+        game.camera.deadzone = new Phaser.Rectangle(150, 150, 500, 300);
+        game.camera.focusOnXY(0, 0);
+
+
     }
 
-    background.tilePosition.x = -game.camera.x;
-    background.tilePosition.y = -game.camera.y;
+    function update() {
+        player.move();
+        background.tilePosition.x = -game.camera.x;
+        background.tilePosition.y = -game.camera.y;
 
-    //  Position all the parts and align rotations
-    shadow.x = player.x;
-    shadow.y = player.y;
-    shadow.rotation = player.rotation;
-
-    turret.x = player.x;
-    turret.y = player.y;
+    }
 }
--- app/player.js
+++ app/player.js
@@ -1,9 +1,44 @@
-var Player = (function() {
-    function Player() {
+Player = (function() {
+    function Player(game, cursors) {
+        //  The base of our player
+        this.game = game;
+        this.sprite = this.game.add.sprite(0, 0, 'sprite', 'tank1');
+        this.sprite.anchor.setTo(0.5, 0.5);
+        this.sprite.animations.add('move', ['tank1', 'tank2', 'tank3', 'tank4', 'tank5', 'tank6'], 20, true);
 
+        this.game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+        this.sprite.body.drag.set(0.2);
+        this.sprite.body.maxVelocity.setTo(400, 400);
+        this.sprite.body.collideWorldBounds = true;
+
+        this.sprite.bringToTop();
+
+        this.cursors = cursors;
+        this.currentSpeed = 0;
     };
 
-    Player.prototype.move() {
+    Player.prototype.move = function() {
+        if (this.cursors.left.isDown) {
+            this.sprite.angle -= 4;
+        } else if (this.cursors.right.isDown) {
+            this.sprite.angle += 4;
+        }
+
+        if (this.cursors.up.isDown) {
+            //  The speed we'll travel at
+            this.currentSpeed = 200;
+        } else {
+            if (this.currentSpeed > 0) {
+                this.currentSpeed -= 2;
+            }
+        }
+
+        if (this.currentSpeed > 0) {
+            this.game.physics.arcade.velocityFromRotation(this.sprite.rotation, this.currentSpeed, this.sprite.body.velocity);
+        } else if (this.currentSpeed == 0) {
+            this.game.physics.arcade.velocityFromRotation(this.sprite.rotation, 0, this.sprite.body.velocity);
+        }
 
     };
+    return Player;
 })();
